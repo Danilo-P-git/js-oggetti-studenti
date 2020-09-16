80367143723c39ecffa92fe4452aa596c5565fdb
Danilo Patanè <d.patane98@gmail.com>
2020-09-16 11:53:04 +0200

    Final refinition

--- js/script.js
+++ js/script.js
@@ -15,8 +15,6 @@ for (var k in studObject) {
 
 
 
-// Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
-
 
 
 // Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
